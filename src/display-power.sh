786ba56e74dc66b194502895383f3510146614e7
Kipjr <martijn@koetsier.dev>
2023-04-08 15:35:34 +0200

    add display check for every mqtt request

--- src/display-power.sh
+++ src/display-power.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
-
+DISPLAY=$(ps -aux | grep -Pe 'Xorg'| grep -Po '(?<= )(:[0-9]+) ')
+declare -x DISPLAY=$DISPLAY
 
 case $1 in
 
    off)
-     xset s off #turn screensaver permanent off
+     xset -display $DISPLAY s off #turn screensaver permanent off
      ;;
 
    reset)
-    xset s reset #turn screensaver off
+    xset -display $DISPLAY s reset #turn screensaver off
      ;;
 
    on)
-    xset s on #turn screensaver permanent on
+    xset -display $DISPLAY s on #turn screensaver permanent on
      ;;
 
    activate)
-    xset s activate #turn screensaver on
+    xset -display $DISPLAY s activate #turn screensaver on
     ;;
 
   default)
-    xset s 60 60  #turn screensaver on after 60 sec
-    xset s on  #turn screensaver on
+    xset -display $DISPLAY s 60 60  #turn screensaver on after 60 sec
+    xset -display $DISPLAY s on  #turn screensaver on
     ;;
 
 
   longon)
-    xset s 900 900 #turn screensaver on after 15min
-    xset s on #turn screensaver on
+    xset -display $DISPLAY s 900 900 #turn screensaver on after 15min
+    xset -display $DISPLAY s on #turn screensaver on
      ;;
 
 
