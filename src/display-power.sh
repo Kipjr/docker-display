332d19aca6b911daf16e537d726604b508a20b53
Kipjr <martijn@koetsier.dev>
2023-09-24 10:06:39 +0200

    Fix regex & missing commands

--- src/display-power.sh
+++ src/display-power.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DISPLAY=$(ps -aux | grep -Pe 'Xorg'| grep -Po '(?<= )(:[0-9]+) ')
+DISPLAY=$(ps -aux | grep -Pe 'Xorg'| grep -Po '(?<= )(:[0-9]+)(?=\ )')
 declare -x DISPLAY=$DISPLAY
 
 case $1 in
@@ -31,5 +31,25 @@ case $1 in
     xset -display $DISPLAY s on #turn screensaver on
      ;;
 
+  status)
+    xset q #get status
+     ;;
+
+  dpmson)
+    +dpms # EnergyStar Features on
+    ;;
+
+  dpmsoff)
+    -dpms # EnergyStar Features off
+    ;;
 
 esac
+
+# Screen Saver:
+#  prefer blanking:  yes    allow exposures:  yes
+#  timeout:  0    cycle:  60
+
+# DPMS (Energy Star):
+#  Standby: 0    Suspend: 0    Off: 0
+#  DPMS is Enabled
+#  Monitor is On
